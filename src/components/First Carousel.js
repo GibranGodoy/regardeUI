0ce2f21f7e86a7383852f055e0a3e981b6a4ec8a
Kify <klores.valona@gmail.com>
2021-10-18 22:36:40 -0500

    changes after merge

--- src/components/First Carousel.js
+++ src/components/First Carousel.js
@@ -2,23 +2,20 @@ import React from "react";
 import Section from "../containers/carousel/Section";
 import CarouselTemplte from "../containers/carousel/CarouselTemplate";
 import { data } from "../data";
-import './FirstCarousel.scss'
+import "./FirstCarousel.scss";
 
 const FirstCarousel = () => {
   return (
     <div>
-      <Section title={"Descubre más pelis"} />
       <div className="information">
-          <div className="info-title">
-            <h5>Descubre más películas</h5>
-          </div>
-          <div className="more-info">
-            <h6>
-              <span className="underline">Populares</span>{" "}
-              <span>Recientes</span>
-            </h6>
-          </div>
+        <Section title={"Descubre más pelis"} />
+        <div className="more-info">
+          <h6>
+            <span className="underline">Populares</span> <span>Recientes</span>
+          </h6>
         </div>
+      </div>
+
       <CarouselTemplte bd={data}></CarouselTemplte>
     </div>
   );
